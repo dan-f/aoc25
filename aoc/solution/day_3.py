d574dd391866eb6ea08fc701a07e8b141d82a81d
Daniel Friedman <dfriedman58@gmail.com>
2025-12-13 13:41:52 -0500

    day 3

---
+++ aoc/solution/day_3.py
@@ -0,0 +1,33 @@
+from io import TextIOWrapper
+
+
+def part_1(input: TextIOWrapper):
+    joltage = 0
+    for line in input:
+        line = line.strip()
+        first_i, first_c = max(enumerate(line[:-1]), key=lambda x: int(x[1]))
+        second_c = max(line[first_i + 1 :], key=int)
+        joltage += int(f"{first_c}{second_c}")
+    return joltage
+
+
+def part_2(input: TextIOWrapper):
+    joltage = 0
+    for line in input:
+        line = line.strip()
+        digits = [0] * 12
+        start = 0
+        end = len(line) - 11
+        for d in range(12):
+            max_digit = int(line[start])
+            max_i = start
+            for i in range(start + 1, end):
+                digit = int(line[i])
+                if digit > max_digit:
+                    max_digit = digit
+                    max_i = i
+            digits[d] = str(max_digit)
+            start = max_i + 1
+            end = end + 1
+        joltage += int("".join(digits))
+    return joltage
